1d4bbfd4625b0a74fff36c468561df5ce5325aa4
Your Name <you@example.com>
2022-03-11 17:01:45 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -136,7 +136,8 @@ Ejercicio 8.
 Use las operaciones implementadas de vectores para 
 sumar (a1,a2)+(b1+b2). Almacene el resultado en el vector c.
 */
-void sumaV2(int a1, int a2, int b1, int b2, Vector *c)
+void sumaV2(int a1, int a2, int b1, int b2, Vector *c) //Suma de Vectores.
 {
-   printf("haskja");
+   c->datos[0] = a1 + b1; //Coordenadas X
+   c->datos[1] = a2 + b2; //Coordenadas Y
 }
